34271bcac4c17ee7a7a5c9066c0848a54725da33
castle <castle@castleui-iMac.local>
2021-09-29 20:32:50 +0900

    #1.4 Getting the Weather

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "23693b921e547d132d970a62e657f566";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    await console.log(data);
   };
   getLocation = async () => {
     try {
@@ -14,9 +24,10 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
-      this.setState({ isLoading: true });
-
       // Send to API and get Weather
+      this.getWeather(latitude, longitude);
+
+      this.setState({ isLoading: true });
     } catch (error) {
       Alert.alert("Can't find you", "so sad");
     }
